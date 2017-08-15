713ecf7791b0b9e6ec4df8de70b22c4732dc087a
Brian <brian@mageplaza.com>
2017-08-15 17:37:54 +0700

    Fix compile bug

--- Helper/Data.php
+++ Helper/Data.php
@@ -3,10 +3,6 @@
 namespace Mageplaza\GoogleAnalytics\Helper;
 
 use Mageplaza\Core\Helper\AbstractData;
-use Magento\Store\Model\StoreManagerInterface;
-use Magento\Framework\ObjectManagerInterface;
-use Magento\Framework\App\Helper\Context;
-use Magento\Store\Model\ScopeInterface;
 
 class Data extends AbstractData
 {
@@ -16,28 +12,6 @@ class Data extends AbstractData
 	const XML_PATH_GENERAL_LINK_ATTRIBUTION = 'googleanalytics/general/link_attribution';
 	const XML_PATH_GENERAL_IP_ANONYMIZATION = 'googleanalytics/general/ip_anonymization';
 	const XML_PATH_GENERAL_DEBUG = 'googleanalytics/general/debug';
-	protected $storeManager;
-	protected $objectManager;
-
-	public function __construct(
-		Context $context,
-		ObjectManagerInterface $objectManager,
-		StoreManagerInterface $storeManager
-	)
-	{
-		$this->objectManager = $objectManager;
-		$this->storeManager  = $storeManager;
-		parent::__construct($context);
-	}
-
-	public function getConfigValue($field, $storeId = null)
-	{
-		return $this->scopeConfig->getValue(
-			$field,
-			ScopeInterface::SCOPE_STORE,
-			$storeId
-		);
-	}
 
 	public function isEnabled($storeId = null)
 	{
